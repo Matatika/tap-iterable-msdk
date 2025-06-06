fdba6db8440691fd2dada228fbb013191d235944
Reuben <60552974+ReubenFrankel@users.noreply.github.com>
2025-06-06 14:18:05 +0100

    Clarify buffered campaign id flush

--- tap_iterable/streams.py
+++ tap_iterable/streams.py
@@ -84,12 +84,16 @@ class CampaignsStream(IterableStream):
 
     @override
     def parse_response(self, response):
+        last_record = None
         for record in super().parse_response(response):
+            last_record = record
             yield record
 
         # make sure we process the remaining buffer entries
         self._campaign_ids_buffer.finalize()
-        yield record  # yield last record again to force child context generation
+        # yield last record again to force child context generation
+        if last_record is not None:
+            yield last_record
 
     @override
     def generate_child_contexts(self, record, context):
